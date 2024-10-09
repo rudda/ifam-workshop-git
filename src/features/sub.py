a890597d899e5dc04545bd27bfd6ac75432bd0e2
Diego Raposo <diego.eimec@gmail.com>
2024-10-09 09:46:58 -0400

    feat: "função de subtrair dois números" Criei a função que recebe dois numeros como parâmetros e retorna a-b

---
+++ src/features/sub.py
@@ -0,0 +1,10 @@
+def sub(a,b)
+     """
+    Função para subtrair dois números.
+    
+    :param a: Primeiro número
+    :param b: Segundo número
+    :return: A subtração de a e b
+    """
+
+    return a-b
