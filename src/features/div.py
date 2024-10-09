9fd3c886c932acd542535e5b6d76260e23045fa7
Diego Raposo <diego.eimec@gmail.com>
2024-10-09 09:16:47 -0400

    feat: "Função de dividir dois números" Crie uma função que recebe dois numeros como parâmetro a e b e retorna a/b DIEGO

---
+++ src/features/div.py
@@ -0,0 +1,10 @@
+def div(a,b)
+     """
+    Função para dividir dois números.
+    
+    :param a: Primeiro número
+    :param b: Segundo número
+    :return: A divisão de a e b
+    """
+
+    return a/b
