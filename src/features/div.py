d666d9c27c52cc77266040321579456aa5909eb9
mariellemar <mariellemarsantos@gmail.com>
2024-10-09 10:46:22 -0400

    Resolvendo conflitos

--- src/features/div.py
+++ src/features/div.py
@@ -1,16 +1,5 @@
-# src/features/add.py
-
-def div(a, b):
-    """
-    Função para somar dois números.
-    
-    :param a: Primeiro número
-    :param b: Segundo número
-    :return: A soma de a e b
-    """
-    if (b!=0):
-        res = a/b
+def div (a, b):
+    if b == 0:
+        return None
     else:
-        res = None
-
-    return res
+        return a / b
