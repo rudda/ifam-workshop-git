ee31b3a6687349dd74ffedc7caf2f788749e86e4
herculesfj <hercules@icomp.ufam.edu.br>
2024-10-09 11:13:58 -0400

    comentário removido

--- src/features/div.py
+++ src/features/div.py
@@ -1,13 +1,7 @@
 # src/features/div.py
 
 def div(a, b):
-    """
-    Função para somar dois números.
     
-    :param a: Primeiro número
-    :param b: Segundo número
-    :return: A soma de a e b
-    """
     if b == 0:
         return None
     else:
