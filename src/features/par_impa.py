26382fa4cd6ab4e8f8a5b1816774343742042d7b
Brena Pacheco Cidade <brenacidade@gmail.com>
2024-10-09 11:50:24 -0400

    Update par_impa.py

--- src/features/par_impa.py
+++ src/features/par_impa.py
@@ -1,16 +1,22 @@
 def par_ou_impar():
     try:
-        n = int(input("Digite um número inteiro: "))  # Captura a entrada do usuário e tenta converter para inteiro
+        n = float(input("Digite um número inteiro: "))  # Captura a entrada do usuário como float
         
-        if n == 0:
-            return "Erro: O número não pode ser zero."
-        
-        if n % 2 == 0:
-            return "par"
+        # Verifica se o número é inteiro
+        if n.is_integer():
+            n = int(n)  # Converte para inteiro
+            
+            if n == 0:
+                return "Erro: O número não pode ser zero."
+            
+            if n % 2 == 0:
+                return "par"
+            else:
+                return "ímpar"
         else:
-            return "ímpar"
+            return "Erro: A entrada deve ser um número inteiro."
     except ValueError:
-        return "Erro: A entrada deve ser um número inteiro."
+        return "Erro: A entrada deve ser um número."
 
 # Chamando a função e exibindo o resultado
 resultado = par_ou_impar()
