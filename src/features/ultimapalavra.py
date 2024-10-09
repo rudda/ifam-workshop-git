187ac9584753d98861f5235efbf9f6d86eea22ec
tonhoIcomp <antonio.fernandes@icomp.ufam.edu.br>
2024-10-09 11:27:56 -0400

    resolvido o comprimento da última palavra

--- src/features/ultimapalavra.py
+++ src/features/ultimapalavra.py
@@ -4,7 +4,6 @@ def comprimento_ultima_palavra(s: str) -> int:
 
     return len(palavras[-1])
 
-# Testando os exemplos
 print(comprimento_ultima_palavra("Sabriza Frazão"))
 print(comprimento_ultima_palavra("   workshop LG ZLACademy"))  
 print(comprimento_ultima_palavra("Olá mundo!")) 
