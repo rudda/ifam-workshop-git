fa5e81b5f4678eda408e6a4ccd8e5e5afbee9d26
Lucas Andrade <academicolucas>
2024-10-09 09:15:06 -0400

    feat: Adicionei um método de divisão adicionado
    Criei uma função div que recebe dois parâmetros, e retorna a divisão a/b
    
    Lucas

---
+++ src/features/sub.py
@@ -0,0 +1,11 @@
+# src/features/add.py
+
+def sub(a, b):
+    """
+    Função para somar dois números.
+    
+    :param a: Primeiro número
+    :param b: Segundo número
+    :return: A subtracao de a e b
+    """
+    return a - b
