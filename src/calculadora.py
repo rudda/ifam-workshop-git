548e22f954f72c5b53df0202c71da022d1b4abb9
Jade Santos <santmjad@gmail.com>
2024-10-09 17:19:29 -0400

    Adicionar verificação numero impar ou par

--- src/calculadora.py
+++ src/calculadora.py
@@ -1,7 +1,8 @@
 def parimpar(n):
     if n % 2 == 0:
-        return f'{n} é par'
+        return f'{n} é divisível por 2 e é par'
     else:
-        return f'{n} é impar'
+        return f'{n} não é divisível por 2 e é ímpar'
 
-print(parimpar(10))
+print(parimpar(10))  
+print(parimpar(7))   
