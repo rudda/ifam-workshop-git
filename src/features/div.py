d26c7df988b77bebc09717467b035dcb3d65cf91
jonasjss <jonas.santos2302@outlook.com>
2024-10-09 09:20:18 -0400

    feat: Criar função de divisão, multiplicação e sub Pronto pra teste

---
+++ src/features/div.py
@@ -0,0 +1,11 @@
+# src/features/div.py
+
+def div(a, b):
+    """
+    Função para dividir dois números.
+    
+    :param a: Primeiro número
+    :param b: Segundo número
+    :return: A divisão de a e b
+    """
+    return a / b
---
+++ src/features/mult.py
@@ -0,0 +1,11 @@
+# src/features/mult.py
+
+def mult(a, b):
+    """
+    Função para multiplicação dois números.
+    
+    :param a: Primeiro número
+    :param b: Segundo número
+    :return: A multiplicação de a e b
+    """
+    return a * b
